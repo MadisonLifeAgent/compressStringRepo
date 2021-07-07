8dc3967642bb879400b5b22c77f6717af3b3452d
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-07 14:26:20 -0500

    Init

---
+++ compressedString.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// 3.	Compress a string of characters
+    // a.	For example, an input of "aaabbbbbccccaacccbbbaaabbbaaa" would compress to "3a5b4c2a3c3b3a3b3a"
+
+// initialize string
+let theString = "aaabbbbbccccaacccbbbaaabbbaaa";
+
+// function to help compress string
+function compressString(theString){
+    let newString = "";
+
+    // iterate through string to compress and create new string
+    for (i = 0; i < theString.length; i++){
+
+    }
+}
+
+console.log(theString);
